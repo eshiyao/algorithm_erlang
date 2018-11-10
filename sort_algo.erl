6a5a271deac14a618f2084c7595021e3ccd8893a
eshiyao <yaotobe@163.com>
2018-11-10 11:41:14 +0800

    add bubblesort

--- sort_algo.erl
+++ sort_algo.erl
@@ -1,7 +1,27 @@
 -module(sort_algo).
--export([quicksort/1]).
+-export([quicksort/1,
+	 bubblesort/1]).
 
 quicksort([])->
     [];
 quicksort([H|T])->
     quicksort([S||S<-T,S<H])++[H]++quicksort([L||L<-T,L>=H]).
+
+%% bubble sort, each round will fix a largist element
+%% at the end of the list 
+bubblesort(List)->
+    bubblesort(List,length(List)).
+
+bubblesort(List,1)->
+    List;
+bubblesort([H|T],N)->
+    ResultList=compareElement(H,T),
+    bubblesort(ResultList,N-1).
+
+%%internal fun of bubblesort
+compareElement(Refer,[])->
+    [Refer];
+compareElement(Refer,[H|T]) when Refer>H ->
+    [H]++compareElement(Refer,T);
+compareElement(Refer,[H|T])->
+    [Refer]++compareElement(H,T).
